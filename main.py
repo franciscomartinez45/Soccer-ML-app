6fa229c326ef1ad4d3dfca1b3f8abfc032d48c7c
Francisco Martinez <fmartinez110@toromail.csudh.edu>
2025-02-24 15:36:09 -0800

    data correctly formatted

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import boto3
 from soccer_data_api import SoccerDataAPI
+from collections import defaultdict
 
 s3 = boto3.client('s3')
 
@@ -22,37 +23,55 @@ league="laliga"
 country="spain"
 sport="soccer"
 week = 26
-service = Service("/usr/bin/chromedriver")  
-driver = webdriver.Chrome(service=service, options=options)
+
 
 def get_form():
+    service = Service("/usr/bin/chromedriver")  
+    driver = webdriver.Chrome(service=service, options=options)
     URL = f"https://www.flashscoreusa.com/{sport}/{country}/{league}/results/"
     driver.get(URL)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     driver.quit()
     matches = []
-
+    team_form = defaultdict(list)
     """Scrape all information of team form for past 5 days"""
 
-    for match in soup.find_all("div", class_="event__match event__match--withRowLink event__match--static event__match--twoLine")[:50]:
+    for match in soup.find_all("div", class_="event__match event__match--withRowLink event__match--static event__match--twoLine"):
         home_team = match.find("div", class_="wcl-participant_7lPCX event__homeParticipant").text
         away_team = match.find("div", class_="wcl-participant_7lPCX event__awayParticipant").text
         home_score= match.find("div", class_="event__score event__score--home").text
         away_score = match.find("div", class_="event__score event__score--away").text
-      
-        matches.append({"Home Team": home_team, "Away Team": away_team, "Home Score ": home_score,"Away Score":away_score})
+        home_team = ''.join(filter(str.isalpha,home_team))
+        away_team = ''.join(filter(str.isalpha, away_team))
+        matches.append({"home_team": home_team, "away_team": away_team, "home_score": home_score,"away_score":away_score})
+        #print(f'{home_team} {home_score} - {away_score} {away_team}')
+        if home_score > away_score:
+            home_result, away_result = 1, -1
+        elif home_score < away_score:
+            home_result, away_result = -1, 1
+        else:
+            home_result, away_result = 0, 0
+        
+        #print(f"{home_team}, {away_team}")
+        team_form[home_team].append(home_result)
+        team_form[away_team].append(away_result)
+        team_form[home_team] = team_form[home_team][:5]
+        team_form[away_team] = team_form[away_team][:5]
+    
+    #print(matches)
 
     """convert to panda"""
-    df = pd.DataFrame(matches)
-    print(df)
+    df = pd.DataFrame.from_dict(team_form,orient="index")
+    
+    df=df.sort_index()
 
     csv_buffer = StringIO()
-    df.to_csv(csv_buffer, index=False)
-
+    df.to_csv(csv_buffer, index=True, header=False)
+    
     """S3 bucket initializer"""
     bucket_name = 'soccer-predictor-web-scrape' 
     file_name = 'last_five_form.csv' 
-
+    
     
     s3.put_object(
         Bucket=bucket_name,
@@ -86,7 +105,8 @@ def get_standings():
     print(f"Successfully uploaded to s3://{bucket_name}/{file_name}")
    
 def get_fixtures():
-    
+    service = Service("/usr/bin/chromedriver")  
+    driver = webdriver.Chrome(service=service, options=options) 
     standingsURL = f"https://www.flashscoreusa.com/{sport}/{country}/{league}/fixtures/"
     driver.get(standingsURL)
     soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -98,11 +118,11 @@ def get_fixtures():
        
         home_team = match.find("div","wcl-participant_7lPCX event__homeParticipant").text 
         away_team = match.find("div","wcl-participant_7lPCX event__awayParticipant").text
-        teams.append({"Home team":home_team, "Away team":away_team})
+        teams.append({"home_team":home_team, "away_team":away_team})
     df = pd.DataFrame(teams)
     csv_buffer = StringIO()
-    df.to_csv(csv_buffer, index=False)
-    print(df)
+    df.to_csv(csv_buffer)
+    
     """S3 bucket initialization"""
     bucket_name = 'soccer-predictor-web-scrape' 
     file_name = 'upcoming_fixtures.csv' 
